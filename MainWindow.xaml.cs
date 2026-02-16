09c62bfd006e3eaa15b8cde77d44667ef1d1a8bd
FilipRadgowski <filip.radgowski@tm1.edu.pl>
2026-02-16 20:22:02 +0100

    build: First commit of The Project
    Pushed the first stable version of the app.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace TimeTravelPanel {
+    public partial class MainWindow : Window {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
